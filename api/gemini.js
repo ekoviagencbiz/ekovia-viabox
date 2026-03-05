8b7a1dd9edc22b73d0bf464f4846e0b70defcde8
ekoviagencbiz <ekoviagencbiz@gmail.com>
2026-03-05 09:06:07 +0300

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
     }
 
     const url =
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=" +
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" +
       encodeURIComponent(apiKey);
 
     const r = await fetch(url, {
